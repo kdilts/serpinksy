c86d93a6df11c1a14a94d1607f07410a06b3074c
Kevin Dilts <kdilts13@gmail.com>
2016-10-19 14:41:43 -0600

    created empty project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Serpinsky</title>
+		<script src="script.js" type="text/javascript" rel="script"></script>
+	</head>
+	<body>
+		<canvas id="myCanvas"></canvas>
+	</body>
+</html>
